7eaf9710bc751f0feed8e6b89823a634859bcc2e
Mohamed Saad <50009562+mylove90@users.noreply.github.com>
2023-11-08 14:30:21 +0200

    Check for NICOBAR tags

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-KERNEL_SOURCE=https://github.com/RahulPalXDA/kernel_samsung_j2y18lte.git
-BRANCH=stock
-KERNEL_VERSION=msm-3.18
-PLATFORM=*-89xx.0
+KERNEL_SOURCE=https://github.com/Genom-Project/android_kernel_xiaomi_ginkgo.git
+BRANCH=sdm-10-R
+KERNEL_VERSION=msm-4.14
+PLATFORM=*-NICOBAR.0
